42d1467d5267f2459311271bbd24077f936c90c5
pierous <pierous@Laptop>
2013-12-20 00:51:51 +0100

    Solventando errores gramaticales

--- src/main/java/es/udc/proyectogit/web/services/AppModule.java
+++ src/main/java/es/udc/proyectogit/web/services/AppModule.java
@@ -46,7 +46,7 @@ public class AppModule
         // locales to just "en" (English). As you add localised message catalogs and other assets,
         // you can extend this list of locales (it's a comma separated series of locale names;
         // the first locale name is the default when there's no reasonable match).
-        configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en");
+        configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en, es, gl, fr");
         //configuration.add(SymbolConstants.SUPPORTED_LOCALES, supportedLanguages.getCodes());
     }
 
